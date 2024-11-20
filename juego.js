a76eee1bd0a83dfe34c5dc1e620d756f3728e723
Gonzalo <1832888@alu.murciaeduca.es>
2024-11-20 12:30:38 +0100

    Fondo + objeto nave se pintan al pulsar el boton Y la nave se puede mover a la izquierda y derecha

--- juego.js
+++ juego.js
@@ -1,59 +1,87 @@
 window.onload = function() {
 
+    //Constantes bordes del canva
     const TOPEARRIBA = 600;
     const TOPEABAJO = 0;
     const TOPEIZQUIERDA = 0;
     const TOPEDERECHA = 400;
 
+    const teclaDrch = 39;
+    const teclaIzq = 37;
+        
+    const canvas = document.getElementById('miCanvas');
+    const ctx = canvas.getContext('2d');
+
+    //Objeto nave
+    let naveX = 180;
+    const naveY = 560;
+    const naveAncho = 40;
+    const naveAltura = 34;
+    let naveVelocidad = 10;
+
+
+    //Imagenes
     const fondo = new Image();
     fondo.src = '/assets/fondo.png';
     const nave = new Image();
     nave.src = '/assets/naveImagen.png';
-
-	let navePlayer;                    // Objeto nave base
-
     
-    //Captura del click del botón de hmtl
-    let botonJuego = document.getElementById("comenzarJuego").onclick = comenzarJuego;
+    // Captura del click del botón de html
+    document.getElementById("comenzarJuego").onclick = comenzarJuego;
 
+    // Función que se ejecuta al hacer click en el botón
+    function comenzarJuego() { 
+        console.log("El juego ha comenzado");
+            dibujarFondo();
+            dibujarNave();
 
-    //funcion nave
-    function crearNave(x_, y_) {
-        this.x = x_;
-        this.y = y_;
-        this.velocidad = 3;
-        
-        this.tamañoX= 40;
-        this.tamañoY= 40;
+            window.addEventListener('keydown', moverNave, false);
+            window.addEventListener('keyup', pararNave, false);
     }
 
-    nave.prototype.Image= nave;
-    nave = new nave(x, y);
-
-    //Funcion para dibujar la nave
-    function dibujarNave() {
-        const canvas = document.getElementById('miCanvas');
-        const ctx = canvas.getContext('2d');
-        nave.onload = function() {
-            ctx.drawImage(nave, 5, 5, 100,100);
-        }
-
-    }
-
-
-    //Dibujar el fondo del canvas
-    const canvas = document.getElementById('miCanvas');
-    const ctx = canvas.getContext('2d');
-    fondo.onload = function() {
+    function dibujarFondo() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(fondo, 0, 0, canvas.width, canvas.height);
     }
 
-    //Función que se ejecuta al hacer click en el botón
-    function comenzarJuego(botonJuego) { 
-        console.log("El juego ha comenzado");
-        crearNave();
-        
+    function dibujarNave() {
+        ctx.fillRect(naveX, naveY, naveAncho, naveAltura);
+        ctx.drawImage(nave, naveX, naveY, naveAncho, naveAltura);
     }
 
+    function moverNave(evt) {
+        switch(evt.keyCode) {
+            // Flecha izq
+            case teclaIzq:
+                if (naveX > TOPEIZQUIERDA) {
+                    naveX -= naveVelocidad;
+                }
+                break;
+            // Flecha derecha
+            case teclaDrch:
+                if (naveX < TOPEDERECHA - naveAncho) {
+                    naveX += naveVelocidad;
+                }
+                break;
+        }
+        dibujarFondo();
+        dibujarNave();
+    }
 
+    function pararNave(evt) {
+        switch(evt.keyCode) {
+            // Flecha izq
+			case 37:
+				teclaIzq = false;
+				break;
+
+			// Flecha derecha
+			case 39:
+				teclaDrch = false;
+				break;
+		}
+        dibujarFondo();
+        dibujarNave();
+	}
+    
 }
