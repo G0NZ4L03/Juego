72769b325d5a485e3dea1c8262c07bc2277a7416
G0NZ4L03 <1832888@alu.murciaeduca.es>
2024-11-25 20:48:27 +0100

    Arreglada velocidad de disparo, modificacion en assets y clase enemigos pendiente "TODO"

--- juego.js
+++ juego.js
@@ -1,17 +1,19 @@
-window.onload = function() {
+window.onload = function () {
 
-// 1. Declaración de variables y constantes globales
+    // 1. Declaración de variables y constantes globales
 
-    //Constantes bordes del canva
+    //Bordes del canva
     const TOPEARRIBA = 600;
     const TOPEABAJO = 0;
     const TOPEIZQUIERDA = 0;
     const TOPEDERECHA = 400;
 
+    //Teclas
     const teclaDrch = 39;
     const teclaIzq = 37;
     const teclaEspacio = 32;
-        
+
+    //Canvas y su contexto
     const canvas = document.getElementById('miCanvas');
     const ctx = canvas.getContext('2d');
 
@@ -20,127 +22,167 @@ window.onload = function() {
     const naveY = 560;
     const naveAncho = 40;
     const naveAltura = 34;
-    let naveVelocidad = 10;
+    let naveVelocidad = 20;
 
     //Objeto disparo
-
     const disparoX = naveX + naveAncho / 2;
     let disparoY = naveY;
-    const disparoAncho = 5;
-    const disparoAltura = 10;
-    const disparoVelocidad = 10;
+    const disparoAncho = 3;
+    const disparoAltura = 15;
+    const disparoVelocidad = 1;
 
     let disparos = [];
+    let existeDisparo = false;
 
-const fondo = new Image();
-fondo.src = '/assets/fondo.png';
+    //Objeto enemigo
+    const enemigoX = 180;
+    const enemigoY = 30;
+    const enemigoAncho = 40;
+    const enemigoAltura = 34;
+    const enemigoVelocidad = 1;
+    
 
-// 2. Definición de la clase nave y sus métodos
-function nave() {
-    this.x = naveX;
-    this.y = naveY;
-    this.ancho = naveAncho;
-    this.altura = naveAltura;
-    this.velocidad = naveVelocidad;
-    this.imagen = new Image();
-    this.imagen.src = '/assets/naveImagen.png';
-}
+    //Imagen de fondo
+    const fondo = new Image();
+    fondo.src = '/assets/fondo.png';
 
-nave.prototype.dibujarNave = function(ctx) {
-    console.log("Dibujando nave en", this.x, this.y);
-    ctx.drawImage(this.imagen, this.x, this.y, this.ancho, this.altura);
-}
-
-nave.prototype.moverNaveDrch = function() {
-    if (this.x < canvas.width - this.ancho) {
-        this.x += this.velocidad;
-        console.log("Me muevo a la drch", this.x);
+    // 2. Definición de la clase nave y sus métodos en el prototipo
+    function nave() {
+        this.x = naveX;
+        this.y = naveY;
+        this.ancho = naveAncho;
+        this.altura = naveAltura;
+        this.velocidad = naveVelocidad;
+        this.imagen = new Image();
+        this.imagen.src = '/assets/nave2.png';
     }
-}
 
-nave.prototype.moverNaveIzq = function() {
-    if (this.x > 0) {
-        this.x -= this.velocidad;
-        console.log("Me muevo a la izq", this.x);
+    nave.prototype.dibujarNave = function (ctx) {
+        ctx.drawImage(this.imagen, this.x, this.y, this.ancho, this.altura);
     }
-}
 
-let miNave = new nave();
-
-// 3. Funciones de dibujo
-function dibujarFondo() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(fondo, 0, 0, canvas.width, canvas.height);
-}
-
-function dibujarNave() {
-    miNave.dibujarNave(ctx);
-}
-
-function dibujarDisparo(disparo) {
-    ctx.fillStyle = 'red';
-    ctx.fillRect(disparo.x, disparo.y, disparoAncho, disparoAltura);
-    disparo.y -= disparoVelocidad;
-}
-
-// 4. Funciones de manejo de eventos
-function keyDownHandler(evt) {
-    console.log("Key down", evt.keyCode);
-    switch (evt.keyCode) {
-        case teclaIzq:
-            miNave.moverNaveIzq();
-            break;
-        case teclaDrch:
-            miNave.moverNaveDrch();
-            break;
+    nave.prototype.moverNaveDrch = function () {
+        if (this.x < canvas.width - this.ancho) {
+            this.x += this.velocidad;
+        }
     }
-    dibujarFondo();
-    dibujarNave();
-}
 
-function keyUpHandler(evt) {
-    switch (evt.keyCode) {
-        case teclaIzq:
-            // Detener movimiento izquierda
-            break;
-        case teclaDrch:
-            // Detener movimiento derecha
-            break;
+    nave.prototype.moverNaveIzq = function () {
+        if (this.x > 0) {
+            this.x -= this.velocidad;
+        }
     }
-}
 
-function disparo(evt) {
-    if (evt.keyCode == teclaEspacio) {
-        let nuevoDisparo = {
-            x: miNave.x + naveAncho / 2 - disparoAncho / 2,
-            y: miNave.y
-        };
-        disparos.push(nuevoDisparo);
-        actualizarDisparos();
+
+    let miNave = new nave();
+
+
+    // 3. Definición de la clase enemigo y sus métodos en el prototipo (COPIA DE NAVE "TODO" CAMBIAR)
+
+    // function nave() {
+    //     this.x = naveX;
+    //     this.y = naveY;
+    //     this.ancho = naveAncho;
+    //     this.altura = naveAltura;
+    //     this.velocidad = naveVelocidad;
+    //     this.imagen = new Image();
+    //     this.imagen.src = '/assets/nave2.png';
+    // }
+
+    // nave.prototype.dibujarNave = function (ctx) {
+    //     ctx.drawImage(this.imagen, this.x, this.y, this.ancho, this.altura);
+    // }
+
+    // nave.prototype.moverNaveDrch = function () {
+    //     if (this.x < canvas.width - this.ancho) {
+    //         this.x += this.velocidad;
+    //     }
+    // }
+
+    // nave.prototype.moverNaveIzq = function () {
+    //     if (this.x > 0) {
+    //         this.x -= this.velocidad;
+    //     }
+    // }
+
+
+    // let miNave = new nave();
+
+    // 4. Funciones de dibujo
+    function dibujarFondo() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(fondo, 0, 0, canvas.width, canvas.height);
     }
-}
 
-function actualizarDisparos() {
-    dibujarFondo();
-    dibujarNave();
-    disparos = disparos.filter(disparo => disparo.y > TOPEABAJO); // Eliminar disparos que han salido del canvas
-    disparos.forEach(dibujarDisparo);
-    if (disparos.length > 0) {
-        requestAnimationFrame(actualizarDisparos);
+    function dibujarNave() {
+        miNave.dibujarNave(ctx);
     }
-}
 
-// 5. Configuración de eventos
-document.getElementById("comenzarJuego").onclick = comenzarJuego;
+    function dibujarDisparo(disparo) {
+        ctx.fillStyle = 'red';
+        ctx.fillRect(disparo.x, disparo.y, disparoAncho, disparoAltura);
+        disparo.y -= disparoVelocidad;
+    }
 
-// 6. Función para comenzar el juego
-function comenzarJuego() { 
-    console.log("El juego ha comenzado");
-    dibujarFondo();
-    dibujarNave();
+    // 5. Funciones de manejo de eventos
+    function keyDown(evt) {
+        switch (evt.keyCode) {
+            case teclaIzq:
+                miNave.moverNaveIzq();
+                console.log("mover izquierda");
+                break;
+            case teclaDrch:
+                miNave.moverNaveDrch();
+                console.log("mover derecha");
+                break;
+            case teclaEspacio:
+                disparo(evt);
+                break;
+        }
+        dibujarFondo();
+        dibujarNave();
+    }
 
-    window.addEventListener('keydown', keyDownHandler, false);
-    window.addEventListener('keyup', keyUpHandler, false);
-    window.addEventListener('keypress', disparo, false);
-}
+    function disparo(evt) {
+        if (evt.keyCode == teclaEspacio) {
+            console.log("funcion disparo llamada");
+            let nuevoDisparo = {
+                x: miNave.x + naveAncho / 2 - disparoAncho / 2,
+                y: miNave.y
+            };
+            disparos.push(nuevoDisparo);
+            if(!existeDisparo){
+                existeDisparo = true;
+                actualizarDisparos();
+            }
+        }
+    }
+
+    function actualizarDisparos() {
+        dibujarFondo();
+        dibujarNave();
+        disparos = disparos.filter(disparo => disparo.y > TOPEABAJO); // Eliminar disparos que han salido del canvas
+        disparos.forEach(dibujarDisparo);
+        if (disparos.length > 0) {
+            requestAnimationFrame(actualizarDisparos);
+        } else {
+            existeDisparo = false;
+        }
+    }
+
+    // 6. Configuración de boton de inicio
+    document.getElementById("comenzarJuego").onclick = comenzarJuego;
+
+    // 7. Función para comenzar el juego
+    function comenzarJuego() {
+        console.log("empieza el juego");
+        dibujarFondo();
+        dibujarNave();
+
+        // Desactivar el botón para evitar múltiples llamadas
+        document.getElementById("comenzarJuego").onclick = null;
+
+        // Añadir el evento keydown
+        window.addEventListener('keydown', keyDown, false);
+    }
 }
